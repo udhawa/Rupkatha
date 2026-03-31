1a2c963f16a36fcea801577b48c31bcd5f6f95f9
Ujjwal Dhawa <ujj.dhawa100@gmail.com>
2026-03-31 13:33:20 +0000

    Changed function

--- src/helper.java
+++ src/helper.java
@@ -1,7 +1,7 @@
 public class helper{
        public int help(int x){
 	 System.out.println("Help");      
-         return x + 1;
+         return x * x;
        }
        public void connectKafka(){
           connect();
